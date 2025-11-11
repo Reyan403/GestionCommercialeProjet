c4e91ae048d06c01ef2f1e6bc3e7719ec19fb1a3
marc0prra <pereira.marco2006@gmail.com>
2025-11-11 22:16:23 +0100

    ajout du front et commencement du back de la page

---
+++ BLL/GestionProduits.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration; // nécessaire pour ConnectionStringSettings
+using BO;
+using DAL;
+
+namespace BLL
+{
+    public class GestionProduits
+    {
+        private static GestionProduits uneGestionProduits;
+
+        public static GestionProduits GetGestionProduits()
+        {
+            if (uneGestionProduits == null)
+                uneGestionProduits = new GestionProduits();
+            return uneGestionProduits;
+        }
+
+        public static void SetchaineConnexion(ConnectionStringSettings chset)
+        {
+            string chaine = chset.ConnectionString;
+            ConnexionBD.GetConnexionBD().SetchaineConnexion(chaine);
+        }
+
+        public static List<ProduitBO> GetProduits()
+        {
+            return ProduitDAO.GetProduits();
+        }
+
+        public static int ModifierProduit(ProduitBO p)
+        {
+            return ProduitDAO.UpdateProduit(p);
+        }
+
+        public static int SupprimerProduit(int code)
+        {
+            return ProduitDAO.DeleteProduit(code);
+        }
+    }
+}
