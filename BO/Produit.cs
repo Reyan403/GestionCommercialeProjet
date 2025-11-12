9157ddc83d043bca9ad0128113c8160ad1f6b62a
SBKF19 <sofiane.benkherouf05@gmail.com>
2025-11-12 10:07:56 +0100

    ajout de ProduitDAO et de sa méthode GetProduitById qui récupère un produit grâce à son id ajout de ConnexionDB permettant de gérer les requêtes SQL

--- BO/Produit.cs
+++ BO/Produit.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProduitBO
+namespace BO
 {
     public class Produit
     {
+        public int IdProduit { get; set; }
+        public string LibelleProduit { get; set; }
+        public double PrixVenteHTProduit { get; set; }
+        public int IdCategorie { get; set; }
+
+        public Produit() { }
+
+        public Produit(int idProduit, string libelleProduit, double prixVenteHTProduit, int idCategorie)
+        {
+            IdProduit = idProduit;
+            LibelleProduit = libelleProduit;
+            PrixVenteHTProduit = prixVenteHTProduit;
+            IdCategorie = idCategorie;
+        }
     }
 }
