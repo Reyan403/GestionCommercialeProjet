73ba3cd56444f8da469c8138be385e851c632bbc
SBKF19 <sofiane.benkherouf05@gmail.com>
2025-11-12 16:11:54 +0100

    dernier

---
+++ BLL/ProduitBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+using DAL;
+
+namespace BLL
+{
+    public class ProduitBLL
+    {
+        private ProduitDAO produitDAO = new ProduitDAO();
+
+        public Produit GetProduitById(int id)
+        {
+            Produit Produit1 =  produitDAO.GetProduitById(id);
+            return Produit1;
+        }
+    }
+}
